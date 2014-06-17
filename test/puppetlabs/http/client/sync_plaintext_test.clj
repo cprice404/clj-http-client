2b61016f935ec1cd525b3ac6d009bb156ee6f38b
Chris Price <chris@puppetlabs.com>
2014-06-16 18:38:37 -0700

    WIP: refactor common logic in tests

--- test/puppetlabs/http/client/sync_plaintext_test.clj
+++ test/puppetlabs/http/client/sync_plaintext_test.clj
@@ -20,6 +20,23 @@
         (add-ring-handler app "/hello")
         context))
 
+(defn basic-test
+  [http-method java-method clj-fn]
+  (testing (format "sync client: HTTP method: '%s'" http-method)
+    (testlogging/with-test-logging
+      (testutils/with-app-with-config app
+        [jetty9/jetty9-service test-web-service]
+        {:webserver {:port 10000}}
+        (testing "java sync client"
+          (let [options (RequestOptions. "http://localhost:10000/hello/")
+                response (java-method options)]
+            (is (= 200 (.getStatus response)))
+            (is (= "Hello, World!" (slurp (.getBody response))))))
+        (testing "clojure sync client"
+          (let [response (clj-fn "http://localhost:10000/hello/")]
+            (is (= 200 (:status response)))
+            (is (= "Hello, World!" (slurp (:body response))))))))))
+
 (deftest sync-client-head-test
   (testlogging/with-test-logging
     (testutils/with-app-with-config app
@@ -36,106 +53,22 @@
           (is (= nil (:body response))))))))
 
 (deftest sync-client-get-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-       [jetty9/jetty9-service test-web-service]
-       {:webserver {:port 10000}}
-       (testing "java sync client"
-         (let [options (RequestOptions. "http://localhost:10000/hello/")
-               response (SyncHttpClient/get options)]
-           (is (= 200 (.getStatus response)))
-           (is (= "Hello, World!" (slurp (.getBody response))))))
-       (testing "clojure sync client"
-         (let [response (sync/get "http://localhost:10000/hello/")]
-           (is (= 200 (:status response)))
-           (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "GET" #(SyncHttpClient/get %) sync/get))
 
 (deftest sync-client-post-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/post options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/post "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "POST" #(SyncHttpClient/post %) sync/post))
 
 (deftest sync-client-put-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/put options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/put "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "PUT" #(SyncHttpClient/put %) sync/put))
 
 (deftest sync-client-delete-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/delete options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/delete "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "DELETE" #(SyncHttpClient/delete %) sync/delete))
 
 (deftest sync-client-trace-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/trace options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/trace "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "TRACE" #(SyncHttpClient/trace %) sync/trace))
 
 (deftest sync-client-options-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/options options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/options "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "OPTIONS" #(SyncHttpClient/options %) sync/options))
 
 (deftest sync-client-patch-test
-  (testlogging/with-test-logging
-    (testutils/with-app-with-config app
-      [jetty9/jetty9-service test-web-service]
-      {:webserver {:port 10000}}
-      (testing "java sync client"
-        (let [options (RequestOptions. "http://localhost:10000/hello/")
-              response (SyncHttpClient/patch options)]
-          (is (= 200 (.getStatus response)))
-          (is (= "Hello, World!" (slurp (.getBody response))))))
-      (testing "clojure sync client"
-        (let [response (sync/patch "http://localhost:10000/hello/")]
-          (is (= 200 (:status response)))
-          (is (= "Hello, World!" (slurp (:body response)))))))))
+  (basic-test "PATCH" #(SyncHttpClient/patch %) sync/patch))
